bf9b3a13661a8659d4a1d0de1ef2f4e0c04160f9
Andy Ge <andy.ge@curiositychina.com>
2019-05-26 05:53:01 +0800

    添加目录2

--- src/components/LinkTree.tsx
+++ src/components/LinkTree.tsx
@@ -4,9 +4,17 @@ import { Anchor } from 'antd';
 
 const { Link } = Anchor;
 let div = document.createElement('div');
-
-function getListTree(els) {
-  let treeNodes: { nodeName: string; text: string; link: string }[] = [];
+interface AnchorLinkModel {
+  nodeName: string;
+  text: string;
+  link: string;
+}
+interface NodeTreeData {
+  data: AnchorLinkModel;
+  children: NodeTreeData[];
+}
+function getNodeList(els) {
+  let treeNodes: AnchorLinkModel[] = [];
   elementMap(els, function(el) {
     if (isHeaderEl(el)) {
       treeNodes.push({
@@ -16,7 +24,7 @@ function getListTree(els) {
       });
     }
     if (el.children) {
-      let temp = getListTree(el.children);
+      let temp = getNodeList(el.children);
       if (temp.length) {
         treeNodes = treeNodes.concat(temp);
       }
@@ -34,11 +42,57 @@ function isHeaderEl(el) {
     .map(i => `H${1 + +i}`)
     .includes(el.nodeName);
 }
+function getHeaderLevel(headerName: string) {
+  return parseInt(headerName.replace(/[^\d]/g, ''));
+}
 
+function getNodeTree(els) {
+  return _getNodeTree(getNodeList(els));
+
+  function _getNodeTree(list): NodeTreeData[] {
+    let result = [];
+    if (list.length) {
+      let helpLevel = getHeaderLevel(list[0].nodeName);
+      let helpIndex = 0;
+      for (let index = 0; index < list.length; index++) {
+        let item = list[index];
+        let curLevel = getHeaderLevel(item.nodeName);
+        let isLast = index == list.length - 1;
+        if (curLevel <= helpLevel && index != helpIndex) {
+          let temp = list.slice(helpIndex + 1, index);
+          result.push({
+            data: list[helpIndex],
+            children: _getNodeTree(temp)
+          });
+          helpLevel = getHeaderLevel(list[index].nodeName);
+          helpIndex = index;
+          if (!isLast) continue;
+        }
+      }
+      let temp = list.slice(helpIndex + 1, list.length);
+      result.push({
+        data: list[helpIndex],
+        children: _getNodeTree(temp)
+      });
+    }
+
+    return result;
+  }
+}
 export default function LinkTree({ md }: { md: string }) {
   let temp = marked(md);
   div.innerHTML = temp;
-  let links = getListTree(div.childNodes);
+
+  let treeData = getNodeTree(div.childNodes);
+  let TreeLink = function(data: NodeTreeData[]) {
+    return data.map(({ children, data }) => {
+      return (
+        <Link href={`#${data.link}`} title={data.text} key={data.link}>
+          {TreeLink(children)}
+        </Link>
+      );
+    });
+  };
 
   return (
     <Anchor
@@ -47,11 +101,7 @@ export default function LinkTree({ md }: { md: string }) {
         e.preventDefault();
       }}
     >
-      {links.map(link => {
-        return (
-          <Link href={`#${link.link}`} title={link.text} key={link.link} />
-        );
-      })}
+      {TreeLink(treeData)}
     </Anchor>
   );
 }
